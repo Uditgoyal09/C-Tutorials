02f4d02231736b51d37d3809a891331533e697f4
Uditgoyal09 <148673974+Uditgoyal09@users.noreply.github.com>
2024-02-09 20:25:40 +0530

    Update Data type in c.cpp

--- Data type in c.cpp
+++ Data type in c.cpp
@@ -1,16 +1,18 @@
-// You are using GCC
-// #include<stdio.h>
-// int main(){
-        // int a= 5;
-        // float b= 1.2;
-        // char charvar= 'S';
-        // double x= 8.23;
+#include<stdio.h>
+int main(){
+        int a= 5;
+        float b= 1.2;
+        char charvar= 'S';
+        double x= 8.23;
         
-        // printf("Integer type: %d\n", a);
-        // printf("float type: %f\n", b);
-        // printf("character type: %c\n",charvar);
-        // printf("double data type: %lf",x);
-        #include <stdio.h>
+        printf("Integer type: %d\n", a);
+        printf("float type: %f\n", b);
+        printf("character type: %c\n",charvar);
+        printf("double data type: %lf",x);
+
+
+}
+       // #include <stdio.h>
 
     // int a;
     // printf("enter number a:");
