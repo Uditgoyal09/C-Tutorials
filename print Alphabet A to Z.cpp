3c0d988f49c4c447ed5424e37eeca7793ba72302
Uditgoyal09 <148673974+Uditgoyal09@users.noreply.github.com>
2024-02-09 20:43:10 +0530

    Add files via upload

---
+++ print Alphabet A to Z.cpp
@@ -0,0 +1,14 @@
+// You are using GCC
+#include <stdio.h>
+int main(){
+    char ch; 
+    ch = 'A';
+    while (ch<='Z'){    //for (ch='a';ch<=z; ch++);
+    printf("%c",ch);
+    ch++;
+    }
+
+    printf("\n");
+    return 0;
+}
+    
